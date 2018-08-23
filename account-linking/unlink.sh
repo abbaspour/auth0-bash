855f6476a6ac2f030c049985b7570398668c3e95
Amin Abbaspour <amin.abbaspour@auth0.com>
2018-08-23 11:12:09 +1000

    domain from access_token

--- account-linking/unlink.sh
+++ account-linking/unlink.sh
@@ -2,15 +2,10 @@
 
 set -euo pipefail
 
-declare -r DIR=$(dirname ${BASH_SOURCE[0]})
-. ${DIR}/.env
-
 function usage() {
     cat <<END >&2
-USAGE: $0 [-e env] [-t tenant] [-d domain] [-a access_token] [-p primary] [-s secondary] [-v|-h]
+USAGE: $0 [-e env] [-a access_token] [-p primary] [-s secondary] [-v|-h]
         -e file        # .env file location (default cwd)
-        -t tenant      # Auth0 tenant@region
-        -d domain      # Auth0 domain
         -a token       # Access Token
         -p user_id     # primary user_id
         -s user_id     # secondary user_id
@@ -18,25 +13,21 @@ USAGE: $0 [-e env] [-t tenant] [-d domain] [-a access_token] [-p primary] [-s se
         -v             # verbose
 
 eg,
-     $0 -t amin01@au -p 'auth0|5b15eef91c08db5762548fd1' -s 'google-oauth2|103723346187275709910'
+     $0 -p 'auth0|5b15eef91c08db5762548fd1' -s 'google-oauth2|103723346187275709910'
 END
     exit $1
 }
 
-declare AUTH0_DOMAIN=''
-declare access_token=''
 declare secondary_userId=''
 declare primary_userId=''
 declare opt_verbose=0
 
 [[ -f ${DIR}/.env ]] && . ${DIR}/.env
 
-while getopts "e:t:d:a:p:s:hv?" opt
+while getopts "e:a:p:s:hv?" opt
 do
     case ${opt} in
         e) source ${OPTARG};;
-        t) AUTH0_DOMAIN=`echo ${OPTARG}.auth0.com | tr '@' '.'`;;
-        d) AUTH0_DOMAIN=${OPTARG};;
         a) access_token=${OPTARG};;
         p) primary_userId=${OPTARG};;
         s) secondary_userId=`echo ${OPTARG} | tr '|' '/'`;;
@@ -46,9 +37,10 @@ do
     esac
 done
 
-[[ -z "${AUTH0_DOMAIN}" ]] && { echo >&2 "ERROR: AUTH0_DOMAIN undefined"; usage 1; }
 [[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined"; usage 1; }
 [[ -z "${primary_userId}" ]] && { echo >&2 "ERROR: primary_userId undefined"; usage 1; }
 [[ -z "${secondary_userId}" ]] && { echo >&2 "ERROR: secondary_userId undefined"; usage 1; }
 
-curl -X DELETE  -H "Authorization: Bearer $access_token" https://${AUTH0_DOMAIN}/api/v2/users/${primary_userId}/identities/${secondary_userId}
+declare -r AUTH0_DOMAIN_URL=$(echo ${access_token} | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
+
+curl -X DELETE  -H "Authorization: Bearer $access_token" ${AUTH0_DOMAIN_URL}api/v2/users/${primary_userId}/identities/${secondary_userId}
