1e0dcad1155ddf22344ae9bd344ecd90bf18a5c1
Amin Abbaspour <amin.abbaspour@auth0.com>
2018-12-12 16:49:07 +1100

    add samples to list and delete MFA per user

---
+++ multifactor/delete-guardian-enrollment.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eo pipefail
+declare -r DIR=$(dirname ${BASH_SOURCE[0]})
+
+declare enrollment_id=''
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-i enrollment_id] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -i id       # Guardian enrollment id 
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0 -i 'push|dev_NBFIwJ1df2rM6loH'
+END
+    exit $1
+}
+
+while getopts "e:a:i:hv?" opt
+do
+    case ${opt} in
+        e) source ${OPTARG};;
+        a) access_token=${OPTARG};;
+        i) enrollment_id=${OPTARG};;
+        v) opt_verbose=1;; #set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
+[[ -z "${enrollment_id}" ]] && { echo >&2 "ERROR: enrollment_id undefined."; usage 1; }
+
+declare -r AUTH0_DOMAIN_URL=$(echo ${access_token} | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
+
+curl -s -H "Authorization: Bearer ${access_token}" \
+    --request DELETE \
+    --url "${AUTH0_DOMAIN_URL}api/v2/guardian/enrollments/${enrollment_id}"
