bead73220917bbab25772de073f7ff25dd35ba0c
Amin Abbaspour <amin.abbaspour@auth0.com>
2018-12-13 16:25:24 +1100

    script to completely reset MFA of given user

---
+++ multifactor/reset-mfa.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eo pipefail
+declare -r DIR=$(dirname ${BASH_SOURCE[0]})
+
+declare user_id=''
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-i user_id] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -i user_id  # user_id, e.g. 'auth0|5b5fb9702e0e740478884234'
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0 -i 'auth0|5b5fb9702e0e740478884234'
+END
+    exit $1
+}
+
+while getopts "e:a:i:hv?" opt
+do
+    case ${opt} in
+        e) source ${OPTARG};;
+        a) access_token=${OPTARG};;
+        i) user_id=${OPTARG};;
+        v) opt_verbose=1;; #set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
+[[ -z "${user_id}" ]] && { echo >&2 "ERROR: user_id undefined."; usage 1; }
+
+declare -r AUTH0_DOMAIN_URL=$(echo ${access_token} | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
+
+curl -s -H "Authorization: Bearer ${access_token}" \
+    --request GET \
+    --url "${AUTH0_DOMAIN_URL}api/v2/users/${user_id}/authenticators" | jq -r '.[].id' | xargs -L1 -I% \
+curl -s -H "Authorization: Bearer ${access_token}" \
+    --request DELETE \
+    --url "${AUTH0_DOMAIN_URL}api/v2/guardian/enrollments/%" 
