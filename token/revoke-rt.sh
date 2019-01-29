1230deff2dc5dc9c6ee63127d047e86b3fb4c8c9
Amin Abbaspour <amin.abbaspour@auth0.com>
2019-01-28 17:28:19 -0800

    grants api & /oauth/revoke

---
+++ token/revoke-rt.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -ueo pipefail 
+
+declare -r DIR=$(dirname ${BASH_SOURCE[0]})
+[[ -f ${DIR}/.env ]] && . ${DIR}/.env
+
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-t tenant] [-d domain] [-c client_id] [-x client_secret] [-r refresh_token ] [-v|-h]
+        -e file        # .env file location (default cwd)
+        -t tenant      # Auth0 tenant@region
+        -d domain      # Auth0 domain
+        -c client_id   # Auth0 client ID
+        -x secret      # Auth0 client secret
+        -r token       # refresh_token
+        -h|?           # usage
+        -v             # verbose
+
+eg,
+     $0 -t amin01@au -c aIioQEeY7nJdX78vcQWDBcAqTABgKnZl -x XXXXXX -r 8pPLmfUcn7zG60RdZYIeFrxcpbDiwfaXy6_R9PSLqJ_iq
+END
+    exit $1
+}
+
+declare AUTH0_DOMAIN=''
+declare AUTH0_CLIENT_ID=''
+declare AUTH0_CLIENT_SECRET=''
+declare opt_verbose=0
+declare refresh_token=''
+
+while getopts "e:t:d:c:x:r:hv?" opt
+do
+    case ${opt} in
+        e) source ${OPTARG};;
+        t) AUTH0_DOMAIN=`echo ${OPTARG}.auth0.com | tr '@' '.'`;;
+        d) AUTH0_DOMAIN=${OPTARG};;
+        c) AUTH0_CLIENT_ID=${OPTARG};;
+        x) AUTH0_CLIENT_SECRET=${OPTARG};;
+        r) refresh_token=${OPTARG};;
+        v) opt_verbose=1;; #set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${AUTH0_DOMAIN}" ]] && { echo >&2 "ERROR: AUTH0_DOMAIN undefined"; usage 1; }
+[[ -z "${AUTH0_CLIENT_ID}" ]] && { echo >&2 "ERROR: AUTH0_CLIENT_ID undefined"; usage 1; }
+[[ -z "${refresh_token}" ]] && { echo >&2 "ERROR: authorization_code undefined"; usage 1; }
+
+declare secret=''
+[[ -n "${AUTH0_CLIENT_SECRET}" ]] && secret="\"client_secret\":\"${AUTH0_CLIENT_SECRET}\","
+
+declare BODY=$(cat <<EOL
+{
+    "client_id":"${AUTH0_CLIENT_ID}",
+    ${secret}
+    "token": "${refresh_token}"
+}
+EOL
+)
+
+curl --request POST \
+  --url https://${AUTH0_DOMAIN}/oauth/revoke \
+  --header 'content-type: application/json' \
+  --data "${BODY}"
+
