be5bd9d3cfffceba96d4658d6af9de16780bca0f
Amin Abbaspour <amin.abbaspour@auth0.com>
2019-05-28 13:41:16 +1000

    added dynamic client registration

---
+++ clients/register-client.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# make sure 
+# 1. "OIDC Dynamic Application Registration" is enabled for your tenant. Visit Manage > Tenant Settings > Advanced
+# 2. desired connection is domain level. PATCH connection to set `is_domain_connection` to true or use update-connection.sh
+
+set -eo pipefail
+declare -r DIR=$(dirname ${BASH_SOURCE[0]})
+
+which awk > /dev/null || { echo >&2 "error: awk not found"; exit 3; }
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-t tenant] [-d domain] [-n name] [-r uri,uri] [-p|-v|-h]
+        -e file         # .env file location (default cwd)
+        -t tenant       # Auth0 tenant@region
+        -d domain       # Auth0 domain
+        -n name         # client name (e.g. "My Client")
+        -r redirect_uri # comma seperated redirect_uris
+        -p              # public client. set token_endpoint_auth_method to none. default is client_secret_post
+        -h|?            # usage
+        -v              # verbose
+
+eg,
+     $0 -n "My App" -r https://jwt.io -p
+END
+    exit $1
+}
+
+declare AUTH0_DOMAIN=''
+declare client_name=''
+declare token_endpoint_auth_method='client_secret_post'
+declare input_redirect_uris=''
+
+while getopts "e:t:d:n:r:phv?" opt
+do
+    case ${opt} in
+        e) source ${OPTARG};;
+        t) AUTH0_DOMAIN=`echo ${OPTARG}.auth0.com | tr '@' '.'`;;
+        d) AUTH0_DOMAIN=${OPTARG};;
+        n) client_name=${OPTARG};;
+        r) input_redirect_uris=${OPTARG};;
+        p) token_endpoint_auth_method='none';;
+        v) opt_verbose=1;; #set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${AUTH0_DOMAIN}" ]] && { echo >&2 "ERROR: AUTH0_DOMAIN undefined."; usage 1; }
+[[ -z "${client_name}" ]] && { echo >&2 "ERROR: client_name undefined."; usage 1; }
+
+declare -r redirect_uris=$(echo ${input_redirect_uris} |  sed 's/,/","/g') 
+
+declare BODY=$(cat <<EOL
+{
+  "client_name": "${client_name}",
+  "redirect_uris": ["${redirect_uris}"],
+  "token_endpoint_auth_method": "${token_endpoint_auth_method}"
+}
+EOL
+)
+
+curl --request POST \
+     --data "${BODY}" \
+     --header 'content-type: application/json' \
+     --url https://${AUTH0_DOMAIN}/oidc/register
+
--- connections/update-connection.sh
+++ connections/update-connection.sh
@@ -10,6 +10,7 @@ USAGE: $0 [-e env] [-a access_token] [-i connect_id] [-f file] [-v|-h]
         -a token    # access_token. default from environment variable
         -i id       # connection_id
         -f file     # connection definition JSON file
+        -d          # make connection domain level
         -h|?        # usage
         -v          # verbose
 
@@ -21,14 +22,16 @@ END
 
 declare json_file=''
 declare connection_id=''
+declare mk_domain=0
 
-while getopts "e:a:i:f:hv?" opt
+while getopts "e:a:i:f:dhv?" opt
 do
     case ${opt} in
         e) source ${OPTARG};;
         a) access_token=${OPTARG};;
         i) connection_id=${OPTARG};;
         f) json_file=${OPTARG};;
+        d) mk_domain=1;;
         v) opt_verbose=1;; #set -x;;
         h|?) usage 0;;
         *) usage 1;;
@@ -37,6 +40,10 @@ done
 
 [[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
 [[ -z "${connection_id}" ]] && { echo >&2 "ERROR: connection_id undefined."; usage 1; }
+if [[ ${mk_domain} -ne 0 ]]; then
+    json_file=`mktemp`
+    echo '{"is_domain_connection": true}' > ${json_file}
+fi
 [[ -z "${json_file}" ]] && { echo >&2 "ERROR: json_file undefined."; usage 1; }
 [[ -f "${json_file}" ]] || { echo >&2 "ERROR: json_file missing: ${json_file}"; usage 1; }
 
