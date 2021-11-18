052792db7214f5ece8e3ac596531d5791d4d7c4c
Amin Abbaspour <a.abbaspour@gmail.com>
2021-11-18 14:34:54 +1100

    enable realm in db connection

---
+++ connections/enable-context-realm.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+### usage in custom-db
+# function login(email, password, context, callback) {
+#    // context.realm
+# }
+set -euo pipefail
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-i connection_id] [-c cert-file] [-p key-file] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -i id       # connection_id
+        -D          # Disable (default is to enable)
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0
+END
+    exit $1
+}
+
+declare connection_id=''
+declare mode=true
+
+while getopts "e:a:i:Ehv?" opt
+do
+    case ${opt} in
+        e) source "${OPTARG}";;
+        a) access_token=${OPTARG};;
+        i) connection_id=${OPTARG};;
+        D) mode=false;;
+        v) set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
+[[ -z "${connection_id}" ]] && { echo >&2 "ERROR: connection_id undefined."; usage 1; }
+
+readonly AUTH0_DOMAIN_URL=$(echo "${access_token}" | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
+
+
+readonly BODY=$(curl --silent --request GET \
+    -H "Authorization: Bearer ${access_token}" \
+    --url "${AUTH0_DOMAIN_URL}api/v2/connections/${connection_id}" \
+    --header 'content-type: application/json' | jq "del(.realms, .id, .strategy, .name, .provisioning_ticket_url) | .options += {\"realm_fallback\": ${mode}, \"enable_script_context\": ${mode} }")
+
+curl --request PATCH \
+    -H "Authorization: Bearer ${access_token}" \
+    --header 'content-type: application/json' \
+    --url "${AUTH0_DOMAIN_URL}api/v2/connections/${connection_id}" \
+    --data "${BODY}"
