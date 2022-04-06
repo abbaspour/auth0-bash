19d5b183dd3bdde8fdb40a3157322637db8eb023
Amin Abbaspour <a.abbaspour@gmail.com>
2022-04-07 09:04:09 +1000

    adding self service list and remove authenticators

---
+++ multifactor/list-self-authenticators.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eo pipefail
+declare -r DIR=$(dirname ${BASH_SOURCE[0]})
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-i user_id] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0 -i 'auth0|5b5fb9702e0e740478884234'
+END
+    exit $1
+}
+
+while getopts "e:a:hv?" opt
+do
+    case ${opt} in
+        e) source ${OPTARG};;
+        a) access_token=${OPTARG};;
+        v) opt_verbose=1;; #set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
+
+declare -r AUTH0_DOMAIN_URL=$(echo "${access_token}" | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
+
+curl -s -H "Authorization: Bearer ${access_token}" \
+    --request GET \
+    --url "${AUTH0_DOMAIN_URL}mfa/authenticators"
---
+++ multifactor/remove-self-authenticators.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eo pipefail
+declare -r DIR=$(dirname ${BASH_SOURCE[0]})
+
+declare authenticator_id=''
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-i user_id] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -i id       # authenticator id, e.g. 'totp|dev_t6eZI7yfdvT7qoko'
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0 -i 'auth0|5b5fb9702e0e740478884234'
+END
+    exit $1
+}
+
+while getopts "e:a:i:hv?" opt
+do
+    case ${opt} in
+        e) source ${OPTARG};;
+        a) access_token=${OPTARG};;
+        i) authenticator_id=${OPTARG};;
+        v) opt_verbose=1;; #set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
+[[ -z "${authenticator_id}" ]] && { echo >&2 "ERROR: authenticator_id undefined."; usage 1; }
+
+declare -r AUTH0_DOMAIN_URL=$(echo ${access_token} | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
+
+curl -s -H "Authorization: Bearer ${access_token}" \
+    --request DELETE \
+    --url "${AUTH0_DOMAIN_URL}mfa/authenticators/${authenticator_id}"
