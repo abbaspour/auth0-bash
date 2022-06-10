d018582a4660806ba0de127ade7f5c92428e2f9e
Thameera Senanayaka <me@thameera.com>
2022-06-10 20:13:56 +1000

    Add attack protection support (#15)

---
+++ attack-protection/get-brute-force.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+##########################################################################################
+# Author: Auth0
+# Date: 2022-06-12
+# License: MIT (https://github.com/auth0/auth0-bash/blob/main/LICENSE)
+##########################################################################################
+
+set -eo pipefail
+
+command -v curl >/dev/null || { echo >&2 "error: curl not found"; exit 3; }
+command -v jq >/dev/null || { echo >&2 "error: jq not found"; exit 3; }
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-v|-h]
+        -e file         # .env file location (default cwd)
+        -a token        # access_token. default from environment variable
+        -h|?            # usage
+        -v              # verbose
+
+eg,
+     $0
+END
+    exit $1
+}
+
+while getopts "e:a:hv?" opt; do
+    case ${opt} in
+    e) source ${OPTARG} ;;
+    a) access_token=${OPTARG} ;;
+    v) set -x;;
+    h | ?) usage 0 ;;
+    *) usage 1 ;;
+    esac
+done
+
+[[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
+
+declare -r AVAILABLE_SCOPES=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .scope' <<< "${access_token}")
+declare -r EXPECTED_SCOPE="read:attack_protection"
+[[ " $AVAILABLE_SCOPES " == *" $EXPECTED_SCOPE "* ]] || { echo >&2 "ERROR: Insufficient scope in Access Token. Expected: '$EXPECTED_SCOPE', Available: '$AVAILABLE_SCOPES'"; exit 1; }
+
+declare -r AUTH0_DOMAIN_URL=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .iss' <<< "${access_token}")
+
+curl -s -H "Authorization: Bearer ${access_token}" \
+    --url ${AUTH0_DOMAIN_URL}api/v2/attack-protection/brute-force-protection | jq .
