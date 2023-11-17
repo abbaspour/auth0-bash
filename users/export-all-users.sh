38a9bf654110b91ba373bb7d1ced7b20c1eadc4f
Amin Abbaspour <a.abbaspour@gmail.com>
2023-11-17 12:37:09 +1100

    export all, improved. wip

--- users/export-all-users.sh
+++ users/export-all-users.sh
@@ -1,36 +1,84 @@
 #!/usr/bin/env bash
 
+command -v curl >/dev/null || { echo >&2 "error: curl not found";  exit 3; }
+command -v jq >/dev/null || {  echo >&2 "error: jq not found";  exit 3; }
+command -v mktemp >/dev/null || {  echo >&2 "error: mktemp not found";  exit 3; }
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-e env] [-a access_token] [-i user_id] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -p prefix   # start end prefix. default is ''
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0 -s pa -e pz
+END
+    exit $1
+}
+
+declare prefix=''
+
+while getopts "e:a:p:hv?" opt; do
+    case ${opt} in
+    e) source ${OPTARG} ;;
+    a) access_token=${OPTARG} ;;
+    p) prefix=${OPTARG} ;;
+    v) opt_verbose=1 ;; #set -x;;
+    h | ?) usage 0 ;;
+    *) usage 1 ;;
+    esac
+done
+
 declare -r AVAILABLE_SCOPES=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .scope' <<< "${access_token}")
 declare -r EXPECTED_SCOPE="read:users"
 [[ " $AVAILABLE_SCOPES " == *" $EXPECTED_SCOPE "* ]] || { echo >&2 "ERROR: Insufficient scope in Access Token. Expected: '$EXPECTED_SCOPE', Available: '$AVAILABLE_SCOPES'"; exit 1; }
 
 declare -r AUTH0_DOMAIN_URL=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .iss' <<<"${access_token}")
 
+readonly tmp=$(mktemp --suffix json)
+
+declare -i total_exported=0
 
 for x in {a..z}; do
   declare -i p=0
   all_done=false
 
+  echo -n "starting with ${prefix}${x} "
   while [[ $all_done != true ]]; do
-
-    echo "users-${x}-${p}.json"
+    declare file="users-${prefix}-${x}-${p}.json"
 
     curl -k -s --get -H "Authorization: Bearer ${access_token}" \
         -H 'content-type: application/json' \
         --data-urlencode "per_page=100" \
         --data-urlencode "page=$p" \
         --data-urlencode "include_totals=true" \
-        --data-urlencode "q=(email:${x}*)" \
-        "${AUTH0_DOMAIN_URL}api/v2/users" | jq '.' > "users-${x}-${p}.json"
+        --data-urlencode "q=(email:${prefix}${x}*)" \
+        "${AUTH0_DOMAIN_URL}api/v2/users" | jq '.' > "${tmp}"
 
-      declare length=$(jq '.length' "users-${x}-${p}.json")
+      declare -i length=$(jq '.length' "${tmp}")
+      declare -i total=$(jq '.total' "${tmp}")
 
-      p=$((p+1))
+      if [[ "${p}" -eq 0 ]]; then
+          [[ "${total}" -ge 1000 ]] && echo "WARNING. maximum reached. run again with: -p ${prefix}${x}"
+          echo -n "(${total}) "
+      fi
+      total_exported=$((total_exported + length))
 
+      jq -r '.users' "${tmp}" > "${file}"
+      echo -n "$p "
       if [[ "${length}" -lt 100 ]]; then all_done=true; fi
+      p=$((p+1))
   done
-
+  echo
 done
 
-#jq .users users-*.json | jq -s '.' > all-users.json
-jq -s '.' users-*.json | jq -r .users > all-users.json
+echo "Total exported: ${total_exported}"
+
+jq -s add users-${prefix}-*.json >> "all-users${prefix}.json"
+
+readonly total_in_file=$(jq length "all-users${prefix}.json")
+
+echo "Total in file : ${total_in_file}"
