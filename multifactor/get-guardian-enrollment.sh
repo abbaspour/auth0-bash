08296276b08d74dc0e9445619823c5314dfda31b
Amin Abbaspour <amin.abbaspour@okta.com>
2026-01-21 09:37:07 +1100

    working on guardian api

--- multifactor/get-guardian-enrollment.sh
+++ multifactor/get-guardian-enrollment.sh
@@ -2,7 +2,7 @@
 
 ##########################################################################################
 # Author: Amin Abbaspour
-# Date: 2022-06-12
+# Date: 2026-01-20
 # License: MIT (https://github.com/abbaspour/auth0-bash/blob/master/LICENSE)
 ##########################################################################################
 
@@ -13,19 +13,19 @@ command -v jq >/dev/null || {  echo >&2 "error: jq not found";  exit 3; }
 
 readonly DIR=$(dirname "${BASH_SOURCE[0]}")
 
-declare user_id=''
+declare enrollment_id=''
 
 function usage() {
     cat <<END >&2
-USAGE: $0 [-e env] [-a access_token] [-i user_id] [-v|-h]
+USAGE: $0 [-e env] [-a access_token] [-i enrollment_id] [-v|-h]
         -e file     # .env file location (default cwd)
         -a token    # access_token. default from environment variable
-        -i user_id  # user_id, e.g. 'auth0|5b5fb9702e0e740478884234'
+        -i id       # Guardian enrollment id
         -h|?        # usage
         -v          # verbose
 
 eg,
-     $0 -i 'auth0|5b5fb9702e0e740478884234'
+     $0 -i 'push|dev_NBFIwJ1df2rM6loH'
 END
     exit $1
 }
@@ -34,7 +34,7 @@ while getopts "e:a:i:hv?" opt; do
     case ${opt} in
     e) source ${OPTARG} ;;
     a) access_token=${OPTARG} ;;
-    i) user_id=${OPTARG} ;;
+    i) enrollment_id=${OPTARG} ;;
     v) opt_verbose=1 ;; #set -x;;
     h | ?) usage 0 ;;
     *) usage 1 ;;
@@ -44,15 +44,13 @@ done
 [[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
 
 declare -r AVAILABLE_SCOPES=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .scope' <<< "${access_token}")
-declare -r EXPECTED_SCOPE="read:users"
+declare -r EXPECTED_SCOPE="read:guardian_enrollments"
 [[ " $AVAILABLE_SCOPES " == *" $EXPECTED_SCOPE "* ]] || { echo >&2 "ERROR: Insufficient scope in Access Token. Expected: '$EXPECTED_SCOPE', Available: '$AVAILABLE_SCOPES'"; exit 1; }
 
-[[ -z "${user_id}" ]] && { echo >&2 "ERROR: user_id undefined."; usage 1; }
+[[ -z "${enrollment_id}" ]] && { echo >&2 "ERROR: enrollment_id undefined."; usage 1; }
 
 declare -r AUTH0_DOMAIN_URL=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .iss' <<<"${access_token}")
 
 curl -s -H "Authorization: Bearer ${access_token}" \
     --request GET \
-    --url "${AUTH0_DOMAIN_URL}api/v2/users/${user_id}/authenticators" | jq .
-
-echo
+    --url "${AUTH0_DOMAIN_URL}api/v2/guardian/enrollments/${enrollment_id}" | jq .
